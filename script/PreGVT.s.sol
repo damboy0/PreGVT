d16819ff072306c51b98a4faa6f6a82a57a46f7c
damboy0 <fagbolade006@gmail.com>
2025-11-16 11:25:15 +0100

    Updates

--- script/PreGVT.s.sol
+++ script/PreGVT.s.sol
@@ -223,10 +223,10 @@ contract SetupPreGVT is Script {
  * @dev Run with: forge script script/DeployPreGVT.s.sol:ConfigurePresale --rpc-url <RPC_URL> --broadcast
  */
 contract ConfigurePresale is Script {
-    address payable PREGVT_ADDRESS = payable(0xD1a8c66b132e63d2A1381F2D1000c3799569f3A4); // UPDATE THIS
+    address payable PREGVT_ADDRESS = payable(0x3B4300D9139bAB1e5F604811F412B6588D0e81e6); // UPDATE THIS
 
     // Presale configuration - UPDATE THESE
-    uint256 constant PRICE_PER_TOKEN = 5e16; // 0.005 USDT per token (add 18 decimals)
+    uint256 constant PRICE_PER_TOKEN = 5e15; // 0.005 USDT per token (add 18 decimals)
     bool constant BADGE_REQUIRED = false; // Set to true if badge holders only
     uint256 constant PER_USER_LIMIT = 0; // 10,000 tokens per user (0 = no limit)
 
@@ -263,7 +263,7 @@ contract ConfigurePresale is Script {
  * @dev Run with: forge script script/DeployPreGVT.s.sol:ActivatePresale --rpc-url <RPC_URL> --broadcast
  */
 contract ActivatePresale is Script {
-    address payable PREGVT_ADDRESS = payable(0xD1a8c66b132e63d2A1381F2D1000c3799569f3A4); // UPDATE THIS
+    address payable PREGVT_ADDRESS = payable(0x3B4300D9139bAB1e5F604811F412B6588D0e81e6); // UPDATE THIS
     bool constant ACTIVATE = true; // Set to false to deactivate
 
     function run() external {
@@ -294,14 +294,14 @@ contract ActivatePresale is Script {
  * @dev Run with: forge script script/DeployPreGVT.s.sol:UpdatePrice --rpc-url <RPC_URL> --broadcast
  */
 contract UpdatePrice is Script {
-    address payable PREGVT_ADDRESS;
-    uint256 constant NEW_PRICE = 0.005 * 1e18; // ADD 18 DECIMALS TO USDT AMOUNT
+    address payable PREGVT_ADDRESS = payable(0x3B4300D9139bAB1e5F604811F412B6588D0e81e6);
+    uint256 constant NEW_PRICE = 5e15; // ADD 18 DECIMALS TO USDT AMOUNT
 
     function run() external {
         require(PREGVT_ADDRESS != address(0), "Update PREGVT_ADDRESS");
         require(NEW_PRICE > 0, "Update NEW_PRICE");
 
-        uint256 priceManagerPrivateKey = vm.envUint("PRICE_MANAGER_PRIVATE_KEY");
+        uint256 priceManagerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         PreGVT preGVT = PreGVT(PREGVT_ADDRESS);
 
